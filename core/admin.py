f47af584341f87d4ec09f4b8cc14acd043089514
Pedro Marques <junnior.pedro.pm@gmail.com>
2021-03-31 20:40:29 -0300

    add MyUser and RegularPlan models

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import RegularPlan, MyUser
 
-# Register your models here.
+admin.site.register(RegularPlan)
+admin.site.register(MyUser)
--- core/models.py
+++ core/models.py
@@ -1,3 +1,50 @@
+import uuid
 from django.db import models
+from django.core.validators import MinValueValidator, MaxValueValidator
+from django.contrib.auth.models import User
 
-# Create your models here.
+CYCLE_CHOICES = [
+    ('daily', 'Daily'),
+    ('weekly', 'Weekly'),
+]
+
+TYPE_CHOICES = [
+    ('bi-time', 'Bi-time'),
+    ('tri-time', 'Tri-time'),
+    ('simple', 'Simple'),
+]
+
+UNIT_CHOICES = [
+    ('kwh', 'kwh'),
+    ('min', 'min'),
+]
+
+
+class RegularPlan(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    name = models.CharField(max_length=150)
+    tar_included = models.BooleanField()
+    subscription = models.FloatField()
+    cycle = models.CharField(max_length=10, choices=CYCLE_CHOICES)
+    type = models.CharField(max_length=10, choices=TYPE_CHOICES)
+    offer_iva = models.BooleanField()
+    off_peak_price = models.FloatField()
+    peak_price = models.FloatField()
+    unit = models.CharField(max_length=3, choices=UNIT_CHOICES)
+    valid = models.BooleanField()
+    publish = models.BooleanField()
+    vat = models.PositiveSmallIntegerField(validators=[MinValueValidator(1), MaxValueValidator(100)])
+    owner = models.BooleanField(null=True, blank=True)
+
+    def __str__(self):
+        return self.name
+
+
+class MyUser(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    plans = models.ManyToManyField(RegularPlan)
+
+    def __str__(self):
+        return self.user.username
+
