a26129024035c6ef03597a2a7f9ef8b3b3412a1e
Pedro Marques <junnior.pedro.pm@gmail.com>
2021-04-06 14:48:07 -0300

    update created_at on MyUser to a DateTimeField

--- core/models.py
+++ core/models.py
@@ -10,7 +10,7 @@ class MyUser(models.Model):
     name = models.CharField(max_length=150)
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     plans = models.ManyToManyField(Plan, blank=True)
-    created_at = models.DateField(auto_now_add=True)
+    created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.user.username
