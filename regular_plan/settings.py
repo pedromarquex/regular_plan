74b1618e6ea5e57e71303129a0ca84365e440b5a
Pedro Marques <junnior.pedro.pm@gmail.com>
2021-04-06 14:47:06 -0300

    add jwt config

--- regular_plan/settings.py
+++ regular_plan/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/3.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
-
+from datetime import timedelta
 from pathlib import Path
 from decouple import config
 
@@ -106,6 +106,13 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+SIMPLE_JWT = {
+    'ACCESS_TOKEN_LIFETIME': timedelta(days=7),
+    'REFRESH_TOKEN_LIFETIME': timedelta(days=21),
+
+    'ALGORITHM': 'HS256',
+    'SIGNING_KEY': SECRET_KEY,
+}
 
 # Internationalization
 # https://docs.djangoproject.com/en/3.1/topics/i18n/
