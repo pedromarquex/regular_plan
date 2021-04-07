5511bdfce0df4581157cfc5361a767d4031050ed
Pedro Marques <junnior.pedro.pm@gmail.com>
2021-04-07 14:30:40 -0300

    change owner field to be wtite_only

--- plans/api/serializers.py
+++ plans/api/serializers.py
@@ -7,3 +7,6 @@ class PlanSerializer(ModelSerializer):
     class Meta:
         model = Plan
         fields = '__all__'
+        extra_kwargs = {
+            'owner': {'write_only': True}
+        }
