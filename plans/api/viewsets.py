29c9502ee5724207cf8ae61d21b4087fb3366c8b
Pedro Marques <junnior.pedro.pm@gmail.com>
2021-04-07 14:30:08 -0300

    update create method on MyPlansViewSet

--- plans/api/viewsets.py
+++ plans/api/viewsets.py
@@ -1,8 +1,10 @@
+from rest_framework import status
+from rest_framework.response import Response
 from rest_framework.viewsets import ModelViewSet
-from plans.models import Plan
 from rest_framework.permissions import IsAuthenticated
 from rest_framework_simplejwt.authentication import JWTAuthentication
 
+from plans.models import Plan
 from .serializers import PlanSerializer
 
 
@@ -20,7 +22,15 @@ class MyPlanViewSet(ModelViewSet):
     authentication_classes = [JWTAuthentication]
     permission_classes = [IsAuthenticated]
     serializer_class = PlanSerializer
-    http_method_names = ['get']
+    http_method_names = ['get', 'post', 'patch']
 
     def get_queryset(self):
         return Plan.objects.filter(owner=self.request.user)
+
+    def create(self, request, *args, **kwargs):
+        request.data['owner'] = request.user.pk
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        self.perform_create(serializer)
+        headers = self.get_success_headers(serializer.data)
+        return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
