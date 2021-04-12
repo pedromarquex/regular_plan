e045410699eb3aec799cf2c716434753ae32075e
Pedro Marques <junnior.pedro.pm@gmail.com>
2021-04-12 18:14:59 -0300

    add core tests

--- core/tests.py
+++ core/tests.py
@@ -1,3 +1,38 @@
-from django.test import TestCase
+from rest_framework import status
+from rest_framework.test import APITestCase
+from .models import MyUser
 
-# Create your tests here.
+
+class UserTest(APITestCase):
+    def test_create_user(self):
+        """
+        ensure we can create a user
+        """
+        url = '/register/'
+        user_data = {
+            "name": "Atila Lamarino",
+            "user": {
+                "username": "oatila",
+                "password": "atilahatescovid"
+            }
+        }
+
+        response = self.client.post(url, user_data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        created_user = MyUser.objects.get(user__username="oatila")
+        self.assertEqual(response.data['id'], str(created_user.id))
+
+    def test_create_user_without_name(self):
+        """
+        ensure we can't create a user without a name
+        """
+        url = '/register/'
+        user_data = {
+            "user": {
+                "username": "oatila",
+                "password": "atilahatescovid"
+            }
+        }
+
+        response = self.client.post(url, user_data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
