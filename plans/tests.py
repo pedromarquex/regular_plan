f7c0549f7516ab69e3ac95c0771db5e08c420a2b
Pedro Marques <junnior.pedro.pm@gmail.com>
2021-04-14 17:36:08 -0300

    remove unused model on plans/tests.py

--- plans/tests.py
+++ plans/tests.py
@@ -1,7 +1,6 @@
 from django.contrib.auth.models import User
 from rest_framework import status
 from rest_framework.test import APITestCase
-from .models import Plan
 from core.models import MyUser
 
 
