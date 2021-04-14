ff6b012e0f72ee83708b613587adab03fd95cfff
Pedro Marques <junnior.pedro.pm@gmail.com>
2021-04-14 01:39:42 -0300

    add a test to ensure that a authenticated user can list all Plans

--- plans/tests.py
+++ plans/tests.py
@@ -1,3 +1,29 @@
-from django.test import TestCase
+from django.contrib.auth.models import User
+from rest_framework import status
+from rest_framework.test import APITestCase
+from .models import Plan
+from core.models import MyUser
 
-# Create your tests here.
+
+class PlanTest(APITestCase):
+    def test_list_plans(self):
+        """
+        ensure we can list all plans with a authenticated user
+        """
+        user_data = {
+            "username": "oatila",
+            "password": "atilahatescovid"
+        }
+        user = User.objects.create_user(**user_data)
+        my_user_data = {
+            "name": "Atila",
+            "user": user
+        }
+        MyUser.objects.create(**my_user_data)
+        auth_url = '/auth/'
+        auth_response = self.client.post(auth_url, user_data, format='json')
+
+        url = '/plans/'
+        self.client.credentials(HTTP_AUTHORIZATION='Bearer ' + auth_response.data['access'])
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
